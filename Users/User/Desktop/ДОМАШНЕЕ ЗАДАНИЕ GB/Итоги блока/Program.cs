7e0ab29557bda9a79d6bfe8a9a1e6ff47960b139
Valery_Krylov <supermaaaan@mail.ru>
2023-02-19 13:33:16 +0500

    Постановка задачи

--- Users/User/Desktop/ДОМАШНЕЕ ЗАДАНИЕ GB/Итоги блока/Program.cs
+++ Users/User/Desktop/ДОМАШНЕЕ ЗАДАНИЕ GB/Итоги блока/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*ЗАДАЧА: Написать программу, которая формирует массив из строк, длина которых <= 3 символа*/ 
