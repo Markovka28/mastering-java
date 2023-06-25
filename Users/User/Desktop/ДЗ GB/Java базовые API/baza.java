8678db607c673d5615d31633012bd1849657a98b
Valery_Krylov <supermaaaan@mail.ru>
2023-06-25 09:40:26 +0500

    additional task

--- Users/User/Desktop/ДЗ GB/Java базовые API/baza.java
+++ Users/User/Desktop/ДЗ GB/Java базовые API/baza.java
@@ -1,33 +1,41 @@
-// Реализовать простой калькулятор
+// (дополнительное задание) Задано уравнение вида q + w = e, q, w, e >= 0. 
+// Некоторые цифры могут быть заменены знаком вопроса, например, 2? + ?5 = 69. 
+// Требуется восстановить выражение до верного равенства. Предложить хотя бы одно решение или сообщить, что его нет.
 
 import java.util.Scanner;
 
 public class baza {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Введите 1е и 2е число -");
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.print("Выберите и введите тип операции которую необходимо выполнить (+, -, *, /, %)- ");
-            char op = sc.next().charAt(0);
-            solve(a, b, op);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Введите уравнение:2? + ?5 = 69 ");
+            String equation = scanner.nextLine();
+            String[] parts = equation.split("\s*[+=]\s*");
+            int q = -1, w = -1, e = -1;
+            for (int i = 0; i < 10; i++) {
+                if (q == -1 && !parts[0].contains("?")) {
+                    q = Integer.parseInt(parts[0]);
+                }
+                if (w == -1 && !parts[1].contains("?")) {
+                    w = Integer.parseInt(parts[1]);
+                }
+                if (e == -1 && !parts[2].contains("?")) {
+                    e = Integer.parseInt(parts[2]);
+                }
+                if (parts[0].charAt(i) == '?') {
+                    q = i;
+                }
+                if (parts[1].charAt(i) == '?') {
+                    w = i;
+                }
+                if (parts[2].charAt(i) == '?') {
+                    e = i;
+                }
+            }
+            if (q + w == e && q != -1 && w != -1 && e != -1) {
+                System.out.printf("%d+%d = %d", q, w, e);
+            } else {
+                System.out.println("Решений нет");
+            }
         }
-    }
-
-    public static int solve(int a, int b, char op) {
-        int ans = 0;
-        if (op == '+') {
-            ans = a + b;
-        } else if (op == '-') {
-            ans = a - b;
-        } else if (op == '*') {
-            ans = a * b;
-        } else if (op == '%') {
-            ans = a % b;
-        } else if (op == '/') {
-            ans = a / b;
-        }
-        System.out.println("Получается: - " + ans);
-        return ans;
     }
 }
